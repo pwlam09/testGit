dddc6b0cca80a5c42c5d40eb7756a7a7ff101c82
pwlam09 <laughgirlidy@gmail.com>
2015-09-28 18:48:11 +0800

    hello2 again

--- test2/src/test2/Main.java
+++ test2/src/test2/Main.java
@@ -6,6 +6,6 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("hello!");
-		System.out.println("hello2!");
+		System.out.println("hello2!"); //hello2 again
 	}
 }
