b1a533612785086aed813c1315aa54896381fcdf
pwlam09 <laughgirlidy@gmail.com>
2017-07-24 00:20:18 +0800

    Test commit in sublime text 3
    * Test commit in sublime text 3
    * Added third line of hello world!

--- test2/src/test2/Main.java
+++ test2/src/test2/Main.java
@@ -7,5 +7,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello!");
 		System.out.println("hello2!"); //hello2 again
+		System.out.println("hello3!"); //hello3 again
 	}
 }
